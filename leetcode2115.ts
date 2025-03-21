6fc6fc8c59a89665f42a1e41e706e88b0ab25ee3
Yuriy Huchok <yuriy.huchok@tui.co.uk>
2025-03-21 15:32:56 +0100

    feat: 2115

--- leetcode2115.ts
+++ leetcode2115.ts
@@ -1,33 +1,34 @@
+const cookBook = new Map<string, string[]>();
+
+function cookable(recipe: string) {
+  const record = cookBook.get(recipe);
+
+  if (record === undefined) {
+    return false;
+  }
+
+  if (record.length === 0) { // If no ingredients required - the recipe is cookable.
+    return true;
+  }
+
+  cookBook.delete(recipe); // Removing requested recipe to avoid infinite loop when recipes require parent recipes.
+
+  const result = record.every((ingredient) => cookable(ingredient));
+
+  if (result === true) {
+    cookBook.set(recipe, []); // Setting no ingredient requirements for this cookable recipe for future requests.
+  }
+
+  return result;
+}
+
+function fillCookBook(recipes: string[], ingredients: string[][], supplies: string[]) {
+  cookBook.clear();
+  [...recipes, ...supplies].forEach((r, i) => cookBook.set(r, ingredients[i] ?? []));
+}
+
 function findAllRecipes(recipes: string[], ingredients: string[][], supplies: string[]): string[] {
-  const recipeMap = new Map<string, string[]>();
-  [...recipes, ...supplies].forEach((r, i) => recipeMap.set(r, ingredients[i] ?? []));
-
-  const cookable = (recipe: string, chain: Set<string> = new Set<string>()) => {
-    const record = recipeMap.get(recipe);
-
-    if (record === undefined) {
-      return false;
-    }
-
-    if (record.length === 0) {
-      return true;
-    }
-
-    if (chain.has(recipe)) {
-      return false;
-    }
-  
-    const result = record.every((ingredient) => cookable(ingredient, chain.add(recipe)));
-
-    if (result === true) {
-      recipeMap.set(recipe, []);
-    } else {
-      recipeMap.delete(recipe);
-    }
-  
-    return result;
-  };
-
+  fillCookBook(recipes, ingredients, supplies);
   return recipes.filter((recipe) => cookable(recipe));
 };
 
