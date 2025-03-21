cda13b1c60daf562d97f1439ded33fe2e810d545
Yuriy Huchok <yuriy.huchok@tui.co.uk>
2025-03-21 18:39:05 +0100

    feat: 13

--- leetcode13.ts
+++ leetcode13.ts
@@ -1,25 +1,28 @@
-const map: Record<string, number> = {
-  "I": 1,
-  "V": 5,
-  "X": 10,
-  "L": 50,
-  "C": 100,
-  "D": 500,
-  "M": 1000,
-  "IV": 4,
-  "IX": 9,
-  "XL": 40,
-  "XC": 90,
-  "CD": 400,
-  "CM": 900,
-};
+/**
+ * Using map because it's faster than usual {"I": 1, ...} object.
+ */
+const map = new Map<string, number>([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+  ["IV", 4],
+  ["IX", 9],
+  ["XL", 40],
+  ["XC", 90],
+  ["CD", 400],
+  ["CM", 900],
+]);
 
 function romanToInt(s: string): number {
   let acc = 0;
 
   for (let i = 0; i < s.length; i++) {
     if (i < s.length -1) {
-      const multiNum = map[`${s[i]}${s[i+1]}`];
+      const multiNum = map.get(`${s[i]}${s[i+1]}`);
 
       if (multiNum !== undefined) {
         acc += multiNum;
@@ -28,7 +31,7 @@ function romanToInt(s: string): number {
       }
     }
 
-    acc += map[s[i]] ?? 0;
+    acc += map.get(s[i]) ?? 0;
   }
 
   return acc;
