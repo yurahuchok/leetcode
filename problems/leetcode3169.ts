1224a0c8ed92ae3a1dcf7befdd8137727bf14a45
Yuri Huchok <yuri.huchok.dev@gmail.com>
2025-03-24 02:52:45 +0100

    feat: 3169 WIP

--- problems/leetcode3169.ts
+++ problems/leetcode3169.ts
@@ -1,13 +1,31 @@
 function countDays(days: number, meetings: number[][]): number {
-  const off: Map<number, boolean> = new Map();
-  
-  meetings.forEach((meeting) => {
-    for (let i = meeting[0]; i <= meeting[1]; i++) {
-      off.set(i, true);
-    }
+  meetings.sort(([start1, end1], [start2, end2]) => {
+    if (start1 < start2) { return -1 }
+    if (start1 > start2) { return 1 }
+    return 0;
   });
 
-  return days - off.size;
+  for (let i = 0; i < meetings.length - 1; i++) {
+    const nextVal = meetings[i + 1][0];
+    for (let n = meetings[i][0]; n <= meetings[i][1]; n++) {
+      if (n >= nextVal) {
+        meetings[i + 1][0] = meetings[i][0];
+        meetings[i + 1][1] = Math.max(meetings[i][1], meetings[i + 1][1]);
+        meetings[i] = [];
+        break;
+      }
+    }
+  }
+
+  const busyDays = meetings.reduce((acc, meeting) => {
+    if (meeting.length === 0) {
+      return acc;
+    }
+
+    return acc + (meeting[1] - meeting[0] + 1);
+  }, 0);
+
+  return days - busyDays;
 };
 
-export default(countDays(10, [[5,7],[1,3],[9,10]]));
+export default(countDays(57, [[3,49],[23,44],[21,56],[26,55],[23,52],[2,9],[1,48],[3,31]]));
