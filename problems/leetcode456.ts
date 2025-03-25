763443e71c0567b633903f685a136bdb9ba99391
Yuri Huchok <yuri.huchok.dev@gmail.com>
2025-03-25 13:01:35 +0100

    feat: 456

--- problems/leetcode456.ts
+++ problems/leetcode456.ts
@@ -3,16 +3,16 @@
  */
 
 function find132pattern(nums: number[]): boolean {
-  let investigations: number[][] = [];
+  let tasks: number[][] = [];
 
   let lowest = undefined;
   let highest = undefined;
 
   for (let i = 0; i < nums.length; i++) {
-    const curr = nums[i];
+    const currentVal = nums[i];
 
-    for (let k = 0; k < investigations.length; k++) {
-      if (curr > nums[investigations[k][0]] && curr < nums[investigations[k][1]]) {
+    for (let k = 0; k < tasks.length; k++) {
+      if (currentVal > tasks[k][0] && currentVal < tasks[k][1]) {
         return true;
       }
     }
@@ -22,32 +22,39 @@ function find132pattern(nums: number[]): boolean {
       continue;
     }
 
-    if (curr < nums[lowest]) {
-      highest = undefined;
+    if (currentVal < nums[lowest]) {
       lowest = i;
+      highest = undefined;
       continue;
     }
 
-    if (curr > nums[lowest] + 1 && (highest === undefined || curr > nums[highest])) {
+    if (currentVal > nums[lowest] + 1 && (highest === undefined || currentVal > nums[highest])) {
       highest = i;
-
       
-      if (investigations.length > 0) {
-        const peek = investigations[investigations.length - 1];
+      const [prevLowest, prevHighest] = tasks.pop() ?? [Infinity, -Infinity];
 
-        if (nums[highest] > nums[peek[1]] && nums[lowest] < nums[peek[0]]) {
-          investigations.pop(); 
-        }
-
-        if (peek[0] === lowest) {
-          investigations.pop();
-        }
+      if (prevLowest <= nums[lowest] && prevHighest >= nums[highest]) {
+        tasks.push([prevLowest, prevHighest]);
+      } else if (nums[lowest] <= prevLowest && nums[highest] >= prevHighest) {
+        tasks.push([nums[lowest], nums[highest]]);
+      } else {
+        tasks.push([prevLowest, prevHighest]);
+        tasks.push([nums[lowest], nums[highest]]);
       }
 
-      investigations.push([lowest, highest]);
       continue;
     }
   }
 
   return false;
 };
+
+export default find132pattern(
+  // [3,5,0,3,3],
+  // [10,15,0,5,8],
+  // [3,5,0,3,1],
+  // [3,5,0,3,4],
+  // [11,14,10,9,8,7,9,8]
+  // [12,15,0,7,9,30]
+  [2,5,-10,-3,-1,20]
+);
